dc2eea626b110165d0674c2362158cab9f8f101c
Rados Kevin <rkevin99@gmail.com>
2021-09-29 12:51:59 +0200

    Created the config file for project (not yet finished)

---
+++ src/protected/config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+// alapvető konfiguráció, itt lesznek a hivatkozások
+
+define('CHARSET','utf-8');
+define('TITLE','Veggies and Fruits Shop');
+define('CSS',['style,css','style2.css']); //Amikor a frontendet csinalod, "rakd be" ide a fileok nevet
+
+// konstansok
+
+define('ROOT_DIR','./');
+define('PROTECTED_DIR',ROOT_DIR.'protected/');
+define('PUBLIC_DIR',ROOT_DIR.'public/');
+define('VIEWS_DIR',PROTECTED_DIR.'views/');
+define('CSS_DIR',PUBLIC_DIR.'css/');
+define('CORE_DIR',PROTECTED_DIR.'core/');
+define('CONTENT_DIR',PROTECTED_DIR.'content/');
+
+define('START_CONTENT','main');
+
+define('BASE_URL','http://localhost/afp2021_1_K0800_csapat4/');
