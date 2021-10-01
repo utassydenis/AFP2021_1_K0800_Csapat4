e93b9d32cf4ce09b8c7ec11bc3b228f715d86a94
Dénes Utassy <utassy.denis@gmail.com>
2021-10-01 17:57:30 +0200

    Added Login Screen php and css files

---
+++ src/Bejelentkezés.php
@@ -0,0 +1,18 @@
+<?php require_once './protected/config/config.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="<?=CHARSET?>">
+    <title><?=TITLE?></title>
+    <?php if(!empty(CSS)){
+        $css_item_count = count(CSS);
+        for($i = 0; $i < $css_item_count; $i++){
+            echo'<link rel="stylesheet" type="text/css" href="'.CSS_DIR.CSS[$i].'">';
+        }
+    } ?>
+</head>
+<body>
+    <?php include PROTECTED_DIR . 'menu.php';?>
+    <?php include PROTECTED_DIR . 'BejelentkezésContent.php';?>
+</body>
+</html>
