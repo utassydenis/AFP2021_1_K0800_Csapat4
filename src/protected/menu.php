86c556409c6fd5758e24f8100b732f4548456ec2
Dénes Utassy <utassy.denis@gmail.com>
2021-10-04 18:56:23 +0200

    Added Menu and MenuCSS

---
+++ src/protected/menu.php
@@ -0,0 +1,26 @@
+<?php
+    $menu = [
+        [
+            'href' => BASE_URL.'start.php',
+            'title' => 'Kezdőoldal'
+        ],
+        [
+            'href' => BASE_URL.'start.php?E=lista',
+            'title' => 'Listázás'
+        ],
+        [
+            'href' => BASE_URL.'start.php?E=kereses&M=kereses',
+            'title' => 'Keresés'
+        ],
+        [
+            'href' => BASE_URL.'start.php?E=hozzaad&M=hozzaad',
+            'title' => 'Termék hozzáadása'
+        ],
+        [
+            'href' => BASE_URL.'start.php?E=muvelet&M=lista',
+            'title' => 'Műveletek'
+        ]
+    ];
+    require_once CORE_DIR.'views.php';
+    print_menu($menu);
+?>
